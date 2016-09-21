513e9a01cfa9e1a0dfac7edc958cd8baf5da1d23
Owner <Owner@DESKTOP-DLMP2BP>
2016-09-20 21:26:47 -0700

    Added my library thing--need to change assorted objects and headers to mine vs. the already-existing ones

---
+++ Project1_Part2/include/Matrix2D.h
@@ -0,0 +1,70 @@
+/* Start Header -------------------------------------------------------
+Copyright Matrix2D.h
+Purpose:  Definition of basic matrix functionality
+Language:  C
+Platform: Windows OS, VS2015 Express for Win. Desktop
+Project: sean.higgins CS529_Matrix2D.h_1
+Author: Sean Higgins, sean.higgins
+Creation date: 9-14-2016
+- End Header --------------------------------------------------------*/
+
+
+#ifndef MATRIX2D_H
+#define MATRIX2D_H
+
+
+#include "Vector2D.h"
+#include <math.h>
+
+typedef struct Matrix2D
+{
+	float m[3][3];
+}Matrix2D;
+
+
+/*
+This function sets the matrix Result to the identity matrix
+*/
+void Matrix2DIdentity(Matrix2D *pResult);
+
+/*
+This functions calculated the transpose matrix of Mtx and saves it in Result
+*/
+void Matrix2DTranspose(Matrix2D *pResult, Matrix2D *pMtx);
+
+/*
+This function multiplies Mtx0 with Mtx1 and saves the result in Result
+Result = Mtx0*Mtx1
+*/
+void Matrix2DConcat(Matrix2D *pResult, Matrix2D *pMtx0, Matrix2D *pMtx1);
+
+/*
+This function creates a translation matrix from x & y and saves it in Result
+*/
+void Matrix2DTranslate(Matrix2D *pResult, float x, float y);
+
+/*
+This function creates a scaling matrix from x & y and saves it in Result
+*/
+void Matrix2DScale(Matrix2D *pResult, float x, float y);
+
+/*
+This matrix creates a rotation matrix from "Angle" whose value is in degree.
+Save the resultant matrix in Result
+*/
+void Matrix2DRotDeg(Matrix2D *pResult, float Angle);
+
+/*
+This matrix creates a rotation matrix from "Angle" whose value is in radian.
+Save the resultant matrix in Result
+*/
+void Matrix2DRotRad(Matrix2D *pResult, float Angle);
+
+/*
+This function multiplies the matrix Mtx with the vector Vec and saves the result in Result
+Result = Mtx * Vec
+*/
+void Matrix2DMultVec(Vector2D *pResult, Matrix2D *pMtx, Vector2D *pVec);
+
+
+#endif
---
+++ Project1_Part2/src/Math2D.c
@@ -0,0 +1,70 @@
+/* Start Header -------------------------------------------------------
+Copyright Math2D.c
+Purpose:  Implementation of basic math / collision functionality
+Language:  C
+Platform: Windows OS, VS2015 Express for Win. Desktop
+Project: sean.higgins CS529_Math2D.c_1
+Author: Sean Higgins, sean.higgins
+Creation date: 9-14-2016
+- End Header --------------------------------------------------------*/
+
+#include "Math2D.h"
+#include "stdio.h"
+
+/*
+This function checks if the point P is colliding with the circle whose
+center is "Center" and radius is "Radius"
+*/
+int StaticPointToStaticCircle(Vector2D *pP, Vector2D *pCenter, float Radius)
+{
+	if(Vector2DSquareDistance(pP, pCenter) > Radius*Radius)
+	{
+		return 0;
+	}
+	return 1;
+}
+
+
+/*
+This function checks if the point Pos is colliding with the rectangle
+whose center is Rect, width is "Width" and height is Height
+*/
+int StaticPointToStaticRect(Vector2D *pPos, Vector2D *pRect, float Width, float Height)
+{
+	if (pPos->x < pRect->x - Width / 2 || pPos->x > pRect->x + Width / 2 || pPos->y < pRect->y - Height / 2 || pPos->y > pRect->y + Height / 2)
+	{
+		return 0;
+	}
+
+	return 1;
+}
+
+/*
+This function checks for collision between 2 circles.
+Circle0: Center is Center0, radius is "Radius0"
+Circle1: Center is Center1, radius is "Radius1"
+*/
+int StaticCircleToStaticCircle(Vector2D *pCenter0, float Radius0, Vector2D *pCenter1, float Radius1)
+{
+	if (Vector2DSquareDistance(pCenter0, pCenter1) > powf(Radius0 + Radius1, 2))
+	{
+		return 0;
+	}
+
+	return 1;
+}
+
+/*
+This functions checks if 2 rectangles are colliding
+Rectangle0: Center is pRect0, width is "Width0" and height is "Height0"
+Rectangle1: Center is pRect1, width is "Width1" and height is "Height1"
+*/
+int StaticRectToStaticRect(Vector2D *pRect0, float Width0, float Height0, Vector2D *pRect1, float Width1, float Height1)
+{
+	if (pRect0->x - Width0 / 2 > pRect1->x + Width1 / 2 || pRect1->x - Width1 / 2 > pRect0->x + Width0 / 2 || pRect0->y - Height0 / 2 > pRect1->y + Height1 / 2 || pRect1->y - Height1 / 2 > pRect0->y + Height0 / 2)
+	{
+		return 0;
+	}
+
+	return 1;
+}
